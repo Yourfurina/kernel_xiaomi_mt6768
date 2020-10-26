6449d625bf02b557a669b5450deec628d7d5600e
VoidDev-0 <dev.avax@protonmail.com>
2020-10-26 13:19:01 +0100

    kernel/features: Tweak for performance
    Based on tytydraco's work.
    
    Signed-off-by: VoidDev-0 <dev.avax@protonmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -17,7 +17,7 @@ SCHED_FEAT(START_DEBIT, true)
  * wakeup-preemption), since its likely going to consume data we
  * touched, increases cache locality.
  */
-SCHED_FEAT(NEXT_BUDDY, false)
+SCHED_FEAT(NEXT_BUDDY, true)
 
 /*
  * Prefer to schedule the task that ran last (when we did
@@ -25,6 +25,12 @@ SCHED_FEAT(NEXT_BUDDY, false)
  * cache locality.
  */
 SCHED_FEAT(LAST_BUDDY, true)
+
+/*
+ * skip buddy i.e task called yield() is always skipped and the
+ * next entity is selected to run irrespective of the vruntime
+ */
+SCHED_FEAT(STRICT_SKIP_BUDDY, false)
 
 /*
  * Consider buddies to be cache hot, decreases the likelyness of a
@@ -44,7 +50,7 @@ SCHED_FEAT(LB_BIAS, true)
 /*
  * Decrement CPU capacity based on time not spent running tasks
  */
-SCHED_FEAT(NONTASK_CAPACITY, true)
+SCHED_FEAT(NONTASK_CAPACITY, false)
 
 /*
  * Queue remote wakeups on the target CPU and process them
